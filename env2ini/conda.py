403e224898f058495549d6b6740649e2a2975ecf
dsm-72 <sumner.magruder@yale.edu>
2023-06-04 17:00:14 -0400

    ignore plugin during testing

--- env2ini/conda.py
+++ env2ini/conda.py
@@ -4,12 +4,14 @@
 __all__ = ['conda_subcommands']
 
 # %% ../nbs/09_conda.ipynb 3
+#| eval: false
 from conda.plugins import hookimpl, CondaSubcommand
 
 # %% ../nbs/09_conda.ipynb 4
 from .commands import run_export_conda_to_ini
 
 # %% ../nbs/09_conda.ipynb 5
+#| eval: false
 @hookimpl
 def conda_subcommands():
     yield CondaSubcommand(name='env2ini', action=run_export_conda_to_ini, summary=run_export_conda_to_ini.__doc__)
